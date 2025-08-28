6eb3d41ab4e3ef83660990a618a9c9f2e88af946
Chefmatteo <matthewngbusiness@gmail.com>
2025-08-28 10:44:46 +0800

    update

---
+++ data/post_1.ts
@@ -0,0 +1,21 @@
+export const newBlogPost: BlogPostContent = {
+    slug: 'new-blog-post',
+    title: 'New Blog Post Title',
+    date: '2025-01-20',
+    authors: ['Author Name'],
+    tags: ['tag1', 'tag2'],
+    image: '/image.jpg',
+    excerpt: 'Blog post excerpt...',
+    content: 'Blog post content...',
+    richContent: [
+        {
+            type: 'paragraph',
+            content: 'This is a paragraph of text.'
+        },
+        {
+            type: 'image',
+            src: '/image.jpg',
+            alt: 'Image description'
+        }
+    ]
+};
---
+++ data/preview_1.ts
@@ -0,0 +1,7 @@
+export const newBlogPreview: BlogPreview = {
+    slug: 'new-blog-post',
+    title: 'New Blog Post Title',
+    date: '2025-01-20',
+    excerpt: 'Blog post excerpt...',
+    image: '/image.jpg'
+  };
